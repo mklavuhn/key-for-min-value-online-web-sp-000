b25b4362e926454606e8f0212d81271c5a10c576
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-05-21 21:26:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,5 +5,6 @@ def key_for_min_value(name_hash)
   low_value = 0 
   low_key = nil
   name_hash.each do |key, value|
-    if low_key == 0 || 
+    if low_key == 0 || value < low_value
+      then
 end
